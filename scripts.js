63c795151ccba3c56874a735077f908df63312e7
KF <khorneflakesdev@gmail.com>
2022-05-01 02:15:16 -0400

    weekend-commit
    todas las caracteristicas principales se implementaron, a excepcion del contador, de momento esta uno automatico, pero falta la parte en la que se ejecute desde que el usuario este sobre la vista de los contadores

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 let ubicacionPrincipal = window.pageYOffset;
 let factor = 0.5
+let cantidad = 0
 window.onscroll = function () {
     let Desplazamiento_Actual = window.pageYOffset;
-    if (window.pageYOffset >= 0 && window.pageYOffset <screen.height*factor) {
+    if (window.pageYOffset >= 0 && window.pageYOffset < screen.height * factor) {
         document.getElementById('navbar').style.backgroundColor = 'transparent'
         document.getElementById('options1').style.color = 'white'
         document.getElementById('options2').style.color = 'white'
@@ -12,10 +13,10 @@ window.onscroll = function () {
 
     }
 
-    else if (ubicacionPrincipal >= Desplazamiento_Actual){
+    else if (ubicacionPrincipal >= Desplazamiento_Actual) {
         document.getElementById('navbar').style.top = '0'
     }
-    else if (window.pageYOffset >= screen.height*factor) {
+    else if (window.pageYOffset >= screen.height * factor) {
         document.getElementById('navbar').style.backgroundColor = 'white'
         document.getElementById('navbar').style.top = '-11vh'
         document.getElementById('options1').style.color = '#081E36'
@@ -24,19 +25,20 @@ window.onscroll = function () {
         document.getElementById('options4').style.color = '#081E36'
         document.getElementById('logo').style.opacity = '50%'
     }
-    // if(ubicacionPrincipal >= Desplazamiento_Actual) {
-    //     document.getElementById('navbar').style.top = '0';
 
-    // }
-
-    // else if (window.pageYOffset > screen.height){
-    //     document.getElementById('navbar').style.backgroundColor = 'blue'
-    // }
-    // else if (window.pageYOffset >=  0 && window.pageYOffset <= screen.height){
-    //     document.getElementById('navbar').style.backgroundColor = 'transparent'
-    // }
-    // else {
-    //     document.getElementById('navbar').style.top = '-11vh';
-    // }
     ubicacionPrincipal = Desplazamiento_Actual;
+
 }
+window.onscroll = function () {
+    if (window.pageYOffset >= 2900) {
+        document.getElementById('numero').textContent = '666';
+    }
+}
+
+let tiempo = setInterval(() => {
+    cantidad += 1
+    numero.textContent = cantidad
+    if (cantidad === 290) {
+        clearInterval(tiempo)
+    }
+}, 10);
