4530580aeccacc71b21ee3c4835b94a6c2a62730
khorneflakes <khorneflakesdev@gmail.com>
2022-05-03 17:02:44 -0400

    finished-countUp

--- scripts.js
+++ scripts.js
@@ -28,29 +28,56 @@ window.onscroll = function () {
     ubicacionPrincipal = Desplazamiento_Actual;
 
 }
-let cantidad = 0
+let cantidad_1 = 0
+let cantidad_2 = 0
+let cantidad_3 = 0
 
 const contadores = {
-    cantidad1: 1200,
-    tiempo1: 300,
-    cantidad2: 600,
-    tiempo2: 400,
-}
-const demo = ( demo2 ) => {
-    return demo2+2;
+    cantidad1: 8000,
+    tiempo1: 0.375,
+    cantidad2: 250,
+    tiempo2: 12,
+    cantidad3: 400,
+    tiempo3: 7.5,
 }
 
-const tiempo = setInterval(() => {
+const tiempo1 = setInterval(() => {
 
     if (window.pageYOffset >= 2800) {
-        cantidad += 1
-        numero1.textContent = cantidad
-        if (cantidad === 1200) {
-            clearInterval(tiempo)
+        cantidad_1 += 10
+        numero1.textContent = '+' + cantidad_1 + ' Productos'
+        if (cantidad_1 >= contadores.cantidad1) {
+            clearInterval(tiempo1)
         }
 
     }
-}, 40);
+}, contadores.tiempo1);
+
+const tiempo2 = setInterval(() => {
+
+    if (window.pageYOffset >= 2800) {
+        cantidad_2 += 1
+        numero2.textContent = '+' + cantidad_2 + ' Empresas beneficiadas'
+        if (cantidad_2 >= contadores.cantidad2) {
+            clearInterval(tiempo2)
+        }
+
+    }
+}, contadores.tiempo2);
+
+const tiempo3 = setInterval(() => {
+
+    if (window.pageYOffset >= 2800) {
+        cantidad_3 += 1
+        numero3.textContent = '+' + cantidad_3 + ' Soluciones'
+        if (cantidad_3 >= contadores.cantidad3) {
+            clearInterval(tiempo3)
+        }
+
+    }
+}, contadores.tiempo3);
+
+
 
 console.log(contadores.tiempo1)
 console.log(demo(1500))
