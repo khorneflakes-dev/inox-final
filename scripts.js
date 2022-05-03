003f6f877f9cb3a49c216debbbf57568db5291cb
khorneflakes <khorneflakesdev@gmail.com>
2022-05-03 11:51:44 -0400

    home-navbar-fixed

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ let factor = 0.5
 window.onscroll = function () {
     let Desplazamiento_Actual = window.pageYOffset;
     if (window.pageYOffset >= 0 && window.pageYOffset < screen.height * factor) {
-        document.getElementById('navbar').style.backgroundColor = 'transparent'
-        document.getElementById('options1').style.color = 'white'
-        document.getElementById('options2').style.color = 'white'
-        document.getElementById('options3').style.color = 'white'
-        document.getElementById('options4').style.color = 'white'
+        // document.getElementById('navbar').style.backgroundColor = 'blue'
+        // document.getElementById('options1').style.color = 'white'
+        // document.getElementById('options2').style.color = 'white'
+        // document.getElementById('options3').style.color = 'white'
+        // document.getElementById('options4').style.color = 'white'
         document.getElementById('logo').style.opacity = '100%'
 
     }
@@ -16,7 +16,7 @@ window.onscroll = function () {
         document.getElementById('navbar').style.top = '0'
     }
     else if (window.pageYOffset >= screen.height * factor) {
-        document.getElementById('navbar').style.backgroundColor = 'white'
+        // document.getElementById('navbar').style.backgroundColor = 'white'
         document.getElementById('navbar').style.top = '-11vh'
         document.getElementById('options1').style.color = '#081E36'
         document.getElementById('options2').style.color = '#081E36'
@@ -45,12 +45,12 @@ const tiempo = setInterval(() => {
     if (window.pageYOffset >= 2800) {
         cantidad += 1
         numero1.textContent = cantidad
-        if (cantidad === 300) {
+        if (cantidad === 1200) {
             clearInterval(tiempo)
         }
 
     }
-}, 4);
+}, 40);
 
 console.log(contadores.tiempo1)
 console.log(demo(1500))
